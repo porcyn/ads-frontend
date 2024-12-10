0c02126b3a6b46abba75566ca37ceed990bf4a75
porcyn <120289268+porcyn@users.noreply.github.com>
2024-12-10 16:09:24 +0700

    Update login.js

--- src/login.js
+++ src/login.js
@@ -7,7 +7,7 @@ const API_URL = "https://ads-backend.up.railway.app";
 
 function Login() {
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:3000/auth/google"; // เรียก Google OAuth
+    window.location.href = "https://ads-backend.up.railway.app"; // เรียก Google OAuth
   };
 
   return (
